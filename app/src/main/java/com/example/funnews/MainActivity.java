1cb269325db65c1b2e251d6246baeb7d695c6c26
KaydenYan <604763307yk@sina.com>
2019-11-26 14:32:09 +0800

    kayden:增加JSON的获取

--- app/src/main/java/com/example/funnews/MainActivity.java
+++ app/src/main/java/com/example/funnews/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.funnews.josnGet.HttpUtil;
--- app/src/main/java/com/example/funnews/josnGet/HttpUtil.java
+++ app/src/main/java/com/example/funnews/josnGet/HttpUtil.java
@@ -1,7 +1,6 @@
 package com.example.funnews.josnGet;
 
 import java.io.IOException;
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
