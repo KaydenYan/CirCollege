819cad51df1d40b0170eb5023a88a2d502b4f804
KaydenYan <604763307yk@sina.com>
2019-11-29 00:28:38 +0800

    kadyen:增加喜欢列表

--- app/src/main/java/com/example/funnews/josnGet/HttpUtil.java
+++ app/src/main/java/com/example/funnews/josnGet/HttpUtil.java
@@ -18,7 +18,7 @@ public class HttpUtil{
     public static void parseJsonWithJsonObject(Response response) throws IOException {
         String responseData=response.body().string();
         json_insert j=new json_insert();
-        j.convertArrayToList(responseData);
         j.initDBbyDatabaseHelper(MyApplication.getGlobalContext());
+        j.convertArrayToList(responseData);
     }
 }
