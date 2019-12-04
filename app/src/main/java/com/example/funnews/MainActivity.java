656c68e3040a58bedcb18dbf8661636ad874a3f7
LuWada <1714390477@qq.com>
2019-12-04 19:29:22 +0800

    lwd

--- app/src/main/java/com/example/funnews/MainActivity.java
+++ app/src/main/java/com/example/funnews/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private SmartRefreshLayout srl;
     private SQLiteDatabase db;
     private DBHelper_toutiao dbHelper;
-    //V1
+    //lwd
     protected boolean useThemestatusBarColor = false;//false状态栏透明，true状态栏使用颜色
     protected boolean useStatusBarColor = true;//false状态栏图标浅色，true状态栏颜色深色
     private String abc;
