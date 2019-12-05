3e9d8e47919f93242c7097659b1bea30e616b6be
KaydenYan <604763307yk@sina.com>
2019-12-05 09:23:39 +0800

    kayden:multpics xml

--- app/src/main/java/com/example/funnews/NewsInfoActivity.java
+++ app/src/main/java/com/example/funnews/NewsInfoActivity.java
@@ -2,19 +2,26 @@ package com.example.funnews;
 
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebView;
 
 public class NewsInfoActivity extends AppCompatActivity {
 
 
     private WebView webView;
+    protected boolean useThemestatusBarColor = false;//false状态栏透明，true状态栏使用颜色
+    protected boolean useStatusBarColor = true;//false状态栏图标浅色，true状态栏颜色深色
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_info);
+        setStatusBar();
         initViews();
     }
 
@@ -35,6 +42,29 @@ public class NewsInfoActivity extends AppCompatActivity {
 
         webView.loadUrl(url);
 
+    }
+    protected void setStatusBar() {//状态栏沉浸，状态栏颜色，状态栏系统图标的深浅色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            //根据上面设置是否对状态栏单独设置颜色
+            if (useThemestatusBarColor) {
+                getWindow().setStatusBarColor(getResources().getColor(R.color.colorTheme));//设置状态栏背景色
+            } else {
+                getWindow().setStatusBarColor(Color.TRANSPARENT);//透明
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        } else {
+            //Toast.makeText(this, "低于4.4的android系统版本不存在沉浸式状态栏", Toast.LENGTH_SHORT).show();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && useStatusBarColor) {//android6.0以后可以对状态栏文字颜色和图标进行修改
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
     }
 
 
