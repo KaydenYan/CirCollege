3544d863427693989853a42e7ae6de8ad7de1529
iamsuper <1047971162@qq.com>
2019-12-07 16:58:31 +0800

    dj:显示不同的item布局  显示不同图片

--- app/src/main/java/com/example/funnews/NewsInfoActivity.java
+++ app/src/main/java/com/example/funnews/NewsInfoActivity.java
@@ -5,17 +5,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 public class NewsInfoActivity extends AppCompatActivity {
 
 
     private WebView webView;
+    private ImageView likeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_info);
         initViews();
+        likeBtn = findViewById(R.id.likeBtn);
+
     }
 
     /**
