71e07f47c2b76cb357517c3970142a70ff987db9
2017leo <794793975@qq.com>
2019-12-17 14:04:46 +0800

    ;ll

--- app/src/main/java/com/example/funnews/RegisterActivity.java
+++ app/src/main/java/com/example/funnews/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -32,6 +33,11 @@ public class RegisterActivity extends AppCompatActivity {
 
     //private String realCode;
     private DBOpenHelper mDBOpenHelper;
+    private TextView yzm1;
+    private EditText pnumber1;
+    private EditText yzmhq1;
+    //private int realcode = 123456;//(int) ((Math.random() * 9 + 1) * 100000);
+    private String realcode=""+123456;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,23 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.signup);
         ButterKnife.bind(this);
         mDBOpenHelper = new DBOpenHelper(this);
+
+        //接收手机验证码
+        yzm1 = findViewById(R.id.yzm);
+        yzm1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                pnumber1 = findViewById(R.id.pnumber);
+                String Pnumber = pnumber1.getText().toString().trim();
+                //Intent intent = new Intent(RegisterActivity.this, loginActivity.class);
+                //startActivity(intent);
+
+            }
+        });
+
+
+
 
         //将验证码用图片的形式显示出来
         //mIvRegisteractivityShowcode.setImageBitmap(Code.getInstance().createBitmap());
@@ -136,11 +159,7 @@ public class RegisterActivity extends AppCompatActivity {
     })
     public void onClick(View view) {
         switch (view.getId()) {
-//            case R.id.iv_registeractivity_back: //返回登录页面
-//                Intent intent1 = new Intent(this, loginActivity.class);
-//                startActivity(intent1);
-//                finish();
-//                break;
+
 //            case R.id.iv_registeractivity_showCode:    //改变随机验证码的生成
 //                mIvRegisteractivityShowcode.setImageBitmap(Code.getInstance().createBitmap());
 //                realCode = Code.getInstance().getCode().toLowerCase();
@@ -150,14 +169,15 @@ public class RegisterActivity extends AppCompatActivity {
                 String username = mEtRegisteractivityUsername.getText().toString().trim();
                 String password = mEtRegisteractivityPassword1.getText().toString().trim();
                 String password2 = mEtRegisteractivityPassword2.getText().toString().trim();
-                //String phoneCode = mEtRegisteractivityPhonecodes.getText().toString().toLowerCase();
+                yzmhq1 = findViewById(R.id.yzmhq);
+                String phoneCode = yzmhq1.getText().toString().trim();
 
                 //判断用户名是否包含中文
                 Pattern p = Pattern.compile("[\\u4e00-\\u9fa5]");
                 Matcher m = p.matcher(username);
                 //注册验证
                 if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)  ) {
-//                    if (phoneCode.equals(realCode)) {
+                    if (phoneCode.equals(realcode)) {
                     //判断用户名中是否含有中文
                     if (m.find()) {
                         Toast.makeText(this,  "用户名中不能包含中文!", Toast.LENGTH_SHORT).show();
@@ -179,9 +199,9 @@ public class RegisterActivity extends AppCompatActivity {
                     startActivity(intent2);
                     finish();
                     Toast.makeText(this,  "验证通过，注册成功", Toast.LENGTH_SHORT).show();
-//                    } else {
-//                        Toast.makeText(this, "验证码错误,注册失败", Toast.LENGTH_SHORT).show();
-//                    }
+                    } else {
+                        Toast.makeText(this, "验证码错误,注册失败", Toast.LENGTH_SHORT).show();
+                    }
                 }else {
                     Toast.makeText(this, "未完善信息，注册失败", Toast.LENGTH_SHORT).show();
                 }
